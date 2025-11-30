bd9851a4ed25b19ba609b973eaadf175361d6455
Nonthasak Laoluerat <nonthasak.l@gmail.com>
2025-11-30 23:12:18 +0700

    test(utils): add card_picker unit tests (deterministic helper)

---
+++ tests/tarot_card_picker_tests.rs
@@ -0,0 +1,10 @@
+#[cfg(test)]
+mod tests {
+    use crate::utils::card_picker;
+
+    #[test]
+    fn returns_3_or_5() {
+        let c = card_picker::pick_card_count();
+        assert!(c == 3 || c == 5, "card_picker must return 3 or 5");
+    }
+}
